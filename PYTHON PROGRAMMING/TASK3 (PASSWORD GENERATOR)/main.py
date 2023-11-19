57e75e5ea90e5a55c7f3ae3b0004a7bf2cf43628
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2023-11-19 18:36:48 +0530

    Made some modifications in Task 3

--- PYTHON PROGRAMMING/TASK3 (PASSWORD GENERATOR)/main.py
+++ PYTHON PROGRAMMING/TASK3 (PASSWORD GENERATOR)/main.py
@@ -12,9 +12,21 @@ def generate_password():
     symbols = ['!','@','#','$','%','^','&','*','(',')','_']
     numbers = ['1','2','3','4','5','6','7','8','9','0']
 
-    password_list = [random.choice(letters) for i in range(random.randint(8, 10))]
-    password_list += [random.choice(symbols) for i in range(random.randint(2, 4))]
-    password_list += [random.choice(numbers) for i in range(random.randint(2, 4))]
+    num_letters = letter_entry.get()
+    if num_letters not in numbers:
+        num_letters = 8
+
+    num_symbols = symbol_entry.get()
+    if num_symbols not in numbers:
+        num_symbols = 3
+    
+    num_numbers = num_entry.get()
+    if num_numbers not in numbers:
+        num_numbers = 3
+
+    password_list = [random.choice(letters) for i in range(num_letters)]
+    password_list += [random.choice(symbols) for i in range(num_symbols)]
+    password_list += [random.choice(numbers) for i in range(num_numbers)]
 
     random.shuffle(password_list)
 
@@ -38,21 +50,21 @@ def save():
         messagebox.showwarning(title="EMPTY ENTRY", message="You left something empty.")
     else:
         try:
-            with open("data.json", "r") as data:
+            with open("TASK3 (PASSWORD GENERATOR)\data.json", "r") as data:
                 # Reading old data
                 previous_data = json.load(data)
                 # Updating new data
                 previous_data.update(new_data)
 
-            with open("data.json", "w") as add_data:
+            with open("TASK3 (PASSWORD GENERATOR)\data.json", "w") as add_data:
                 json.dump(previous_data, add_data, indent=4)
 
         except FileNotFoundError:
-            with open("data.json", "w") as data:
+            with open("TASK3 (PASSWORD GENERATOR)\data.json", "w") as data:
                 json.dump(new_data, data, indent=4)
 
         except json.decoder.JSONDecodeError:
-            with open("data.json", "w") as data:
+            with open("TASK3 (PASSWORD GENERATOR)\data.json", "w") as data:
                 json.dump(new_data, data, indent=4)
 
         finally:
@@ -64,7 +76,7 @@ def save():
 
 def find_password():
     website = website_entry.get()
-    with open("data.json") as data:
+    with open("TASK3 (PASSWORD GENERATOR)\data.json") as data:
         previous_data = json.load(data)
         try:
             fetched_email = previous_data[website]["email"]
@@ -85,33 +97,47 @@ window.config(pady=20, padx=20)
 window.title("Password Manager")
 
 canvas = Canvas(width=200, height=200)
-image = PhotoImage(file="PYTHON PROGRAMMING\TASK3 (PASSWORD GENERATOR)\logo.png")
+image = PhotoImage(file=r"C:\Users\abhis\OneDrive\Desktop\CODSOFT\PYTHON PROGRAMMING\TASK3 (PASSWORD GENERATOR)\logo.png")
 canvas.create_image(100, 100, image=image)
-canvas.grid(column=1, row=0)
+canvas.grid(column=1, row=0, pady=10)
 
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label = Label(text="Website *:")
+website_label.grid(row=1, column=0, pady=10)
 website_entry = Entry(width=18)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, pady=10)
 
-email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
+email_label = Label(text="Email/Username *:")
+email_label.grid(column=0, row=2, pady=10)
 email_entry = Entry(width=36)
-email_entry.insert(0, "abhishekbiradar0207@gmail.com")
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=2, pady=10)
 
-password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
+letter_label = Label(text="Number of Letters:")
+letter_label.grid(column=0, row=3, pady=10)
+letter_entry = Entry(width=20)
+letter_entry.grid(row=3, column=1)
+
+symbol_label = Label(text="Number of symbolacters:")
+symbol_label.grid(column=0, row=4, pady=10)
+symbol_entry = Entry(width=20)
+symbol_entry.grid(row=4, column=1)
+
+num_label = Label(text="Number of Numbers:")
+num_label.grid(column=0, row=5, pady=10)
+num_entry = Entry(width=20)
+num_entry.grid(row=5, column=1)
+
+password_label = Label(text="Password *:")
+password_label.grid(column=0, row=6, pady=10)
 password_entry = Entry(width=18)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=6, column=1, pady=10)
 
 generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=6, column=2, pady=10)
 
 add_button = Button(text="Add", width=36, highlightthickness=0, command=save)
-add_button.grid(column=1, columnspan=2, row=4)
+add_button.grid(column=1, columnspan=2, row=7, pady=10)
 
 search_button = Button(text="Search", width=14, command=find_password)
-search_button.grid(column=2, row=1)
+search_button.grid(column=2, row=1, pady=10)
 
 window.mainloop()
